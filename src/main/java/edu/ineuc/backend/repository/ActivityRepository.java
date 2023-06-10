c6c74a4eb171259f30b0dcced4e25e84a83041a7
Karem Leticia <username@domain.example>
2023-06-10 12:15:42 -0300

    Adicionando endpoint get atividade

--- src/main/java/edu/ineuc/backend/repository/ActivityRepository.java
+++ src/main/java/edu/ineuc/backend/repository/ActivityRepository.java
@@ -3,7 +3,9 @@ package edu.ineuc.backend.repository;
 import edu.ineuc.backend.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     List<Activity> findAllByUserId(Long userId);
+    Optional<Activity> findById(Long activityId);
 }
